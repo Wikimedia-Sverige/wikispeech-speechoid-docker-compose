40f423dda24b6392bdd7e169377e9b432970bde6
Karl Wettin <karl.wettin@wikimedia.se>
2020-05-07 01:50:59 +0200

    new setup using gerrit repos

--- mockup-compose-files/docker-compose-entrypoint.sh
+++ mockup-compose-files/docker-compose-entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-cd wikispeech_mockup
-
 echo "Waiting for all other services to start..."
 
-./wait-for-it.sh -h mishkal -p 8080 -t 60
-./wait-for-it.sh -h mary-tts -p 59125 -t 60
-./wait-for-it.sh -h mary-tts -p 8080 -t 60
-./wait-for-it.sh -h pronlex -p 8787 -t 60
-./wait-for-it.sh -h symbolset -p 8771 -t 60
+/srv/compose/wait-for-it.sh -h mishkal -p 8080 -t 60
+/srv/compose/wait-for-it.sh -h mary-tts -p 59125 -t 60
+/srv/compose/wait-for-it.sh -h mary-tts -p 8080 -t 60
+/srv/compose/wait-for-it.sh -h pronlex -p 8787 -t 60
+/srv/compose/wait-for-it.sh -h symbolset -p 8771 -t 60
 
 echo "Starting Wikispeech mockup."
-python3 bin/wikispeech docker-compose.conf
+cd /srv/wikispeech-mockup
+python3 bin/wikispeech /srv/compose/docker-compose.conf
