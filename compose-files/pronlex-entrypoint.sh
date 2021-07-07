168cdad2cccf7cf82ce37bcafc023dc40ffc3ec1
kalle <karl.wettin@wikimedia.se>
2021-07-07 12:26:29 +0200

    Only wait for MariaDB if used

--- compose-files/pronlex-entrypoint.sh
+++ compose-files/pronlex-entrypoint.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Waiting for all other services to start..."
-
-/srv/compose/wait-for-it.sh -h mariadb -p 3306 -t 120
-
 echo "Starting Pronlex..."
 
 DIR=`pwd`
@@ -17,5 +13,8 @@ cd pronlex
 if [[ -z "${PRONLEX_MARIADB_URI}" ]]; then
   /bin/bash scripts/start_server.sh -a ${DIR}/appdir -e sqlite -p 8787 -r lexserver
 else
+  echo "Waiting for all other services to start..."
+  /srv/compose/wait-for-it.sh -h mariadb -p 3306 -t 120
+
   /bin/bash scripts/start_server.sh -a ${DIR}/appdir -e mariadb -l "${PRONLEX_MARIADB_URI}" -p 8787 -r lexserver
 fi
